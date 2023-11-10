ddd774edf1fd61670ae550c45ad58f8bc2712aac
YuanSai <yuans_70g@163.com>
2023-11-10 15:59:43 +0800

    重新新建vue项目 新增vue的gitignore 登录页搭建完成

--- Vue/bmsv2/src/router/index.js
+++ Vue/bmsv2/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-   
+    {
+      path: '/',
+      name: 'login',
+      component: () =>import("@/views/login.vue")
+    },
+
   ]
 })
 
---
+++ Vue/bmsv2/src/views/login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="loginbody">
+        <div class="loginTitle"> 后台管理系统v2 </div>
+        <div class="loginContent">
+            <div class="loginContent-account">
+                <n-input type="text" placeholder="用户名" round />
+            </div>
+            <div class="loginContent-password">
+                <n-input type="password" show-password-on="mousedown" placeholder="密码" :maxlength="8" round />
+            </div>
+            <div class="loginContent-button">
+                <n-button tertiary type="success">
+                    登录
+                </n-button>
+            </div>
+
+        </div>
+
+        ;
+
+
+    </div>
+</template>
+<script setup >
+import { ref } from 'vue'
+
+console.log("浏览器内窗高度" + window.innerHeight + "px")
+console.log("浏览器内窗宽度" + window.innerWidth + "px")
+
+</script>
